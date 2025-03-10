b47d73ebb5d9ff21abbceb984c5bc7bbf4a5d4d0
yong99000 <wonyong2910@gmail.com>
2025-03-10 19:51:17 +0900

    Add files via upload

---
+++ 소스0310.cpp
@@ -0,0 +1,81 @@
+//#include <stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요\n안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요");
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d개\n", keyboard);
+//	printf("마우스: %d개\n"), mouse);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d + %d = %d\n",
+//		coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A = %c, A = %d\n", A, A);
+//	retrun 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi = %.2Lf, e = %.3f.. \n", pi, e);
+//	return 0;
+//}
