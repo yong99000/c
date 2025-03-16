2a1007ca78173e072a907903255591c5d65c5c6b
yong99000 <wonyong2910@gmail.com>
2025-03-16 22:40:03 +0900

    Add files via upload

---
+++ hw.cpp
@@ -0,0 +1,39 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	float b;
+//	a = 123.45;
+//	b = 200;
+//	printf("a의 값 ==> %d\n", a);
+//	printf("b의 값 ==> %f\n", b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b;
+//	float c, d;
+//	a = 100;
+//	b = a;
+//	c = 111.1f;
+//	d = c;
+//	printf("a, b의 값 ==> %d, %d\n", a, b);
+//	printf("c, d의 값 ==> %5.1f, %5.1f\n", c, d);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c, d;
+//	a = 100 + 100;
+//	b = a + 100;
+//	c = a + b - 100;
+//	d = a + b + c;
+//	printf("a, b, c, d 의 값 ==> %d, %d, %d, %d\n", a, b, c, d);
+//	a = b = c = d = 100;
+//	printf("a, b, c, d 의 값 ==> %d, %d, %d, %d\n", a, b, c, d);
+//	a = 100;
+//	a = a + 200;
+//	printf("a 의 값 ==> %d\n", a);
+//}
