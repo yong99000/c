712d741d172b34d38af3f0c4d906f80eaf27c52f
yong99000 <wonyong2910@gmail.com>
2025-03-19 06:56:45 +0900

    Add files via upload

--- hw.cpp
+++ hw.cpp
@@ -37,3 +37,53 @@
 //	a = a + 200;
 //	printf("a 의 값 ==> %d\n", a);
 //}
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b;
+//	int result;
+//	char k;
+//
+//	printf("첫 번째 계산할 값 ==> ");
+//	scanf("%d", &a);
+//	printf("+ - * / % ==> ");
+//	scanf(" %c", &k);
+//	printf("두 번째 계산할 겂 ==> ");
+//	scanf("%d", &b);
+//
+//	if (k == '+') {
+//		result = a + b;
+//		printf("%d + %d = %d\n", a, b, result);
+//	}
+//
+//	if (k == '-') {
+//		result = a - b;
+//		printf("%d - %d = %d\n", a, b, result);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", 123);
+//	printf("%5d\n", 123);
+//	printf("%05d\n", 123);
+//
+//	printf("%f\n", 123.45);
+//	printf("%7.1f\n", 123.45);
+//	printf("%7.3f\n", 123.45);
+//
+//	printf("%s\n", "Basic-C");
+//	printf("%10s\n", "Basic-C");
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("\n줄 바꿈\n연습 \n");
+//	printf("\t탭키\t연습 \n");
+//	printf("이것을\r덮어씁니다 \n");
+//	printf("\a\a\a삐소리 3번 \n");
+//	printf("글자가 \"강조\"되는 효과 \n");
+//	printf("\\\\\\ 역슬래시 세 개 출력 \n");
+//}
