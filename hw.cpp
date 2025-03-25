7119c7389ea08d7be6fc20beb91dbf2dc3607f35
yong99000 <wonyong2910@gmail.com>
2025-03-25 22:50:06 +0900

    Add files via upload

--- hw.cpp
+++ hw.cpp
@@ -87,3 +87,56 @@
 //	printf("글자가 \"강조\"되는 효과 \n");
 //	printf("\\\\\\ 역슬래시 세 개 출력 \n");
 //}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d", 111+222);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 200;
+//	if (a < 100)
+//	{
+//		printf("100보다 작군요..\n");
+//		printf("거짓이므로 앞의 문장은 안 보이겠죠?\n");
+//	}
+//	printf("프로그램 끝!\n");
+//}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("정수를 입력하세요 ==>");
+//	scanf("%d", &a);
+//	if (a % 2 == 0)
+//	{
+//		printf("짝수를 입력했군요..\n");
+//	}
+//	else
+//	{
+//		printf("홀수를 입력했군요..\n");
+//	}
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 75;
+//	if (a > 50)
+//	{
+//		if (a < 100)
+//		{
+//			printf("50보다 크고 100보다 작군요..\n");
+//		}
+//		else
+//		{
+//			printf("와~~ 100보다 크군요..\n");
+//		}
+//	}
+//	else
+//	{
+//		printf("에게~~ 50보다 작군요..\n");
+//	}
+//}
