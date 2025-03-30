b0ed13b37973ba3b1e103305a07572956a5e4bc6
yong99000 <wonyong2910@gmail.com>
2025-03-30 10:49:51 +0900

    Add files via upload

--- hw.cpp
+++ hw.cpp
@@ -140,3 +140,64 @@
 //		printf("ø°∞‘~~ 50∫∏¥Ÿ ¿€±∫ø‰..\n");
 //	}
 //}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("1 ~ 4 ¡ﬂø° º±≈√«œººø‰ : ");
+//	scanf("%d", &a);
+//	switch (a)
+//	{
+//	case 1:
+//		printf("1¿ª º±≈√«ﬂ¥Ÿ.\n");
+//		break;
+//	case 2:
+//		printf("2∏¶ º±≈√«ﬂ¥Ÿ.\n");
+//		break;
+//	case 3:
+//		printf("3¿ª º±≈√«ﬂ¥Ÿ.\n");
+//		break;
+//	case 4:
+//		printf("4∏¶ º±≈√«ﬂ¥Ÿ.\n");
+//		break;
+//	default:
+//		printf("1 ~ 4 ¡ﬂø° º±≈√«œººø‰.\n");
+//	}
+//}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int year;
+//	printf("ø¨µµ∏¶ ¿‘∑¬«œººø‰ : ");
+//	scanf("%d", &year);
+//
+//	switch (year % 12)
+//	{
+//	case 0:
+//		printf("ø¯º˛¿Ã∂Ï\n"); break;
+//	case 1:
+//		printf("¥ﬂ∂Ï\n"); break;
+//	case 2:
+//		printf("∞≥∂Ï\n"); break;
+//	case 3:
+//		printf("µ≈¡ˆ∂Ï\n"); break;
+//	case 4:
+//		printf("¡„∂Ï\n"); break;
+//	case 5:
+//		printf("º“∂Ï\n"); break;
+//	case 6:
+//		printf("»£∂˚¿Ã∂Ï\n"); break;
+//	case 7:
+//		printf("≈‰≥¢∂Ï\n"); break;
+//	case 8:
+//		printf("øÎ∂Ï\n"); break;
+//	case 9:
+//		printf("πÏ∂Ï\n"); break;
+//	case 10:
+//		printf("∏ª∂Ï\n"); break;
+//	case 11:
+//		printf("æÁ∂Ï\n"); break;
+//	}
+//}
