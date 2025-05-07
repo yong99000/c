ad9328b2b74c474d785b35182250a4fa05f24f00
yong99000 <wonyong2910@gmail.com>
2025-05-07 19:48:43 +0900

    Add files via upload

--- 소스0310.cpp
+++ 소스0310.cpp
@@ -1018,31 +1018,138 @@
 //	}
 //	return 0;
 //}
-#include <stdio.h>
-int main()
-{
-	int i_menu;
-	printf("1.더하기 2.빼기\n");
-	scanf("%d", &i_menu);
-	switch (i_menu)
-	{
-	case 1:
-	{
-		int number1, number2;
-		printf("숫자 두 개를 입력하세요\n");
-		scanf("%d %d", &number1, &number2);
-		printf("%d+%d = %d\n", number1, number2, number1 + number2); break;
-	}
-	case 2:
-	{
-		int number3, number4;
-
-		printf("숫자 두 개를 입력하세요\n");
-		scanf("%d %d", &number3, &number4);
-		printf("%d-%d = %d\n", number3, number4, number3 - number4); break;
-	}
-
-	}
-	return 0;
-	}
-}
+//#include <stdio.h>
+//int main()
+//{
+//	int i_menu;
+//	printf("1.더하기 2.빼기\n");
+//	scanf("%d", &i_menu);
+//	switch (i_menu)
+//	{
+//	case 1:
+//	{
+//		int number1, number2;
+//		printf("숫자 두 개를 입력하세요\n");
+//		scanf("%d %d", &number1, &number2);
+//		printf("%d+%d = %d\n", number1, number2, number1 + number2); break;
+//	}
+//	case 2:
+//	{
+//		int number3, number4;
+//
+//		printf("숫자 두 개를 입력하세요\n");
+//		scanf("%d %d", &number3, &number4);
+//		printf("%d-%d = %d\n", number3, number4, number3 - number4); break;
+//	}
+//
+//	}
+//	return 0;
+//	}
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char command;
+//
+//	printf("w, a, s, d 입력 : ");
+//	scanf("%c", &command);
+//
+//	switch (command)
+//	{
+//	case 'w':
+//		printf("위 방향키를 입력하셨습니다.\n"); break;
+//	case 'a':
+//		printf("좌 방향키를 입력하셨습니다.\n"); break;
+//	case 'd':
+//		printf("우 방향키를 입력하셨습니다.\n"); break;
+//	case 's':
+//		printf("아래 방향키를 입력하셨습니다.\n"); break;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int Appetizer, MainDish, Dessert;
+//
+//	printf("Appetizer 1.캐비어, 2.샐러드, 3.푸아그라 중 선택 : ");
+//	scanf("%d", &Appetizer);
+//	printf("MainDish 1.스테이크, 2.생선요리, 3.양갈비 중 선택 : ");
+//	scanf("%d", &MainDish);
+//	printf("Dessert 1.케잌, 2.아이스크림, 3.초콜릿무스 중 선택 : ");
+//	scanf("%d", &Dessert);
+//	printf("주문한 음식은 ");
+//	switch (Appetizer)
+//	{
+//	case 1:
+//		printf("캐비어, "); break;
+//	case 2:
+//		printf("샐러드, "); break;
+//	case 3:
+//		printf("푸아그라, "); break;
+//	}
+//	switch (MainDish)
+//	{
+//	case 1:
+//		printf("스테이크, "); break;
+//	case 2:
+//		printf("생선요리, "); break;
+//	case 3:
+//		printf("양갈비, "); break;
+//    }
+//	switch (Dessert)
+//	{
+//	case 1:
+//		printf("케잌"); break;
+//	case 2:
+//		printf("아이스크림"); break;
+//	case 3:
+//		printf("초콜릿무스"); break;
+//	}
+//	pritnf(" 입니다.")
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		printf("%3d(%02x) ", i, i);
+//		if (i >= 70 && i < 80) { i++; continue; }
+//		if (i % 10 == 9) printf("\n");
+//		if (i == 93) break;
+//		i++;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	while (0)
+//	{
+//		printf("조건이 참일 경우 반복출력\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	while (1)
+//	{
+//		printf("조건이 참일 경우 반복출력\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int count = 0;
+//	while (count < 3)
+//	{
+//		printf("현재 count : %d\n", count);
+//		printf("count가 3보다 작은 동안 반복\n");
+//		count++;
+//	}
+//	return 0;
+//}
